802e7517e1af2d30e34768a9a5380e7874eb58b2
elenayran <36763284+elenayran@users.noreply.github.com>
2021-06-30 16:51:31 +0300

    level 1 added

--- Staj projesi/Assets/Scripts/Script/cameraManager.cs
+++ Staj projesi/Assets/Scripts/Script/cameraManager.cs
@@ -19,13 +19,8 @@ public class cameraManager : MonoBehaviour
 
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothspeed);
-        transform.position = smoothedPosition;
         
-        transform.position = new Vector3(Mathf.Clamp(smoothedPosition.x, minX, maxX), smoothedPosition.y + 0.7f, smoothedPosition.z);
-
-
-        //target.position.y + 7f
-
+        transform.position = new Vector3(Mathf.Clamp(smoothedPosition.x, minX, maxX), Mathf.Clamp(smoothedPosition.y, minY, maxY), smoothedPosition.z);
 
     }
 
